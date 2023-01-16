9c52fb2430cf864a016498ba03dc08f10d08ec0c
SamMotta <samuelsamuca953@gmail.com>
2023-01-15 21:33:33 -0300

    ReminderCard now is dismissible

--- lib/widgets/reminder_card_widget.dart
+++ lib/widgets/reminder_card_widget.dart
@@ -24,29 +24,62 @@ class _ReminderCardWidgetState extends State<ReminderCardWidget> {
           ),
         );
       },
-      child: Hero(
-        tag: reminder.id,
-        child: Card(
-          color: Colors.red,
-          elevation: 2,
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    const Icon(Icons.check),
-                    const SizedBox(width: 12),
-                    Text(
-                      reminder.title,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  "${reminder.text.substring(0, 120).trim()}...",
-                ),
-              ],
+      child: Dismissible(
+        background: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: Container(
+            padding: const EdgeInsets.only(left: 20),
+            color: Colors.red,
+            alignment: Alignment.centerLeft,
+            child: const Icon(
+              Icons.delete_forever,
+              color: Colors.white,
+              size: 42,
+            ),
+          ),
+        ),
+        secondaryBackground: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: Container(
+            padding: const EdgeInsets.only(right: 20),
+            color: Colors.blue,
+            alignment: Alignment.centerRight,
+            child: const Icon(
+              Icons.archive,
+              color: Colors.white,
+              size: 42,
+            ),
+          ),
+        ),
+        key: Key("${reminder.id}"),
+        child: Hero(
+          tag: reminder.id,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+              side: const BorderSide(color: Colors.black26),
+            ),
+            elevation: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.check),
+                      const SizedBox(width: 12),
+                      Text(
+                        reminder.title,
+                        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    "${reminder.text.substring(0, 120).trim()}...",
+                  ),
+                ],
+              ),
             ),
           ),
         ),
